27e4dde37f3039083ef006ad4afcba342f517743
CarlosDCN <48950589+CarlosCardona953@users.noreply.github.com>
2019-04-04 09:07:45 -0500

    Update Metropolis.cpp

--- Metropolis.cpp
+++ Metropolis.cpp
@@ -1,29 +1,42 @@
 
 
+#include <stdio.h>
+#include <math.h>
+#include <algorithm>  
 
 using std::cout;
 using std::endl;
+using std::min;
 
 void metro(int N);
-void fun(int x);
-float f=0;
+
+double f=0;
+double k=0;
 
 int main(){
     
   srand48(time(0));
   int N=50000;
+  for (i=0; i<N; i++){
       
-  metro(N);
+  metro();    
+  cout << f << endl;
+  }
+  
      
   return 0;
 }
 
-void metro(int N){
-    
-    for (i=0; i<N; i++){
-        float propuesta  = f + (drand48()-0.5);
-        float r = min(1,fun(propuesta)/fun(f));
-        float alpha = drand48();
+void metro(){
+        double x1=0.0;
+        double x2=0.0;       
+
+        double propuesta  = f + (drand48()*2-1);
+        x1= propuesta;
+        x2= f;
+        double k= exp(-x1*x1/2)/exp(-x2*x2/2);
+        double r = min(1.0,k);
+        double alpha = drand48();
             
         if (alpha<r){
             
@@ -35,14 +48,9 @@ void metro(int N){
         }
         
         
-    std::cout << f << std::endl;
-    }
+   
+    
 
-
-}
-
-void fun(float x){
-f= exp(-x*x/2);
 
 }
 
