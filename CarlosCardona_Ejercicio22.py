ebb4e9172e30e1b4068b42e84ca5e5a04ab93fa2
CarlosDCN <48950589+CarlosCardona953@users.noreply.github.com>
2019-04-04 10:07:31 -0500

    Update CarlosCardona_Ejercicio22.py

--- CarlosCardona_Ejercicio22.py
+++ CarlosCardona_Ejercicio22.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 os.system("g++ Metropolis.cpp -o metropolis.x")
 os.system("./metropolis.x > datos.dat")
 
+data= np.loadtxt("datos.dat")
+x= (data[0]*1000)
+plt.figure()
+plt.hist(x, density=True, label='$\sigma_{\delta}=1$', bins=40)
+plt.savefig("Metropolis.png")
